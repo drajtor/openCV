95101ce5a4d3faeb20a44c7d0b4b009586518bfc
drajtor <rajtor@gmail.com>
2024-11-24 21:13:31 +0100

    added extreme tags recognition and highlight

--- apriltagsPlay.py
+++ apriltagsPlay.py
@@ -16,16 +16,6 @@ def init_camera():
 
     return video_capture
 
-def mark_tags_on_screen (frame, tags):
-    for tag in tags:
-#        pose, e0, e1 = tag_detector.detection_pose(tag, camera_parameters, tag_size_in_meters)
-        cv2.line (frame, tuple(map(int,tag.corners[0])), tuple(map(int,tag.corners[1])), (0,255,0), thickness=2) 
-        cv2.line (frame, tuple(map(int,tag.corners[1])), tuple(map(int,tag.corners[2])), (0,255,0), thickness=2) 
-        cv2.line (frame, tuple(map(int,tag.corners[2])), tuple(map(int,tag.corners[3])), (0,255,0), thickness=2) 
-        cv2.line (frame, tuple(map(int,tag.corners[3])), tuple(map(int,tag.corners[0])), (0,255,0), thickness=2) 
-
-        cv2.putText(frame, str(tag.tag_id), (int(tag.center[0]), int(tag.center[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-
 def find_extreme_tags (tags):
 
     if not tags or len(tags) < 4:
@@ -33,10 +23,32 @@ def find_extreme_tags (tags):
 
     tag_right = max(tags, key=lambda t: t.center[0])
     tag_left = min(tags, key=lambda t: t.center[0])
-    tag_top = max(tags, key=lambda t: t.center[1])
+    tag_top = min(tags, key=lambda t: t.center[1])
     tag_bottom = max(tags, key=lambda t: t.center[1])
 
     return tag_right, tag_left, tag_top, tag_bottom
+
+def mark_tags_on_screen (frame, tags):
+    extreme_tags = find_extreme_tags(tags)
+
+    if not extreme_tags:
+        pass #TODO
+
+    extreme_color = (0,0,255)
+    regular_color = (0,255,0)
+
+
+    for tag in tags:
+        if tag in extreme_tags:
+            color = extreme_color
+        else:
+            color = regular_color
+        cv2.line (frame, tuple(map(int,tag.corners[0])), tuple(map(int,tag.corners[1])), color, thickness=2) 
+        cv2.line (frame, tuple(map(int,tag.corners[1])), tuple(map(int,tag.corners[2])), color, thickness=2) 
+        cv2.line (frame, tuple(map(int,tag.corners[2])), tuple(map(int,tag.corners[3])), color, thickness=2) 
+        cv2.line (frame, tuple(map(int,tag.corners[3])), tuple(map(int,tag.corners[0])), color, thickness=2) 
+
+        cv2.putText(frame, str(tag.tag_id), (int(tag.center[0]), int(tag.center[1])), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
 
 def main():
     CAMERA_INDEX = 0
@@ -60,11 +72,8 @@ def main():
         grayFrame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         tags =tag_detector.detect(grayFrame)
 
-    #    print ("detected tags: {}".format(len(tags)))
         mark_tags_on_screen(frame, tags)
-
         cv2.imshow("kaka", frame)
-        #cv2.imshow("kaka", grayFrame) 
 
         if cv2.waitKey(0) == ord('q'):
             break
